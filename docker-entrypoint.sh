d4069874c833eba85fb68a890a9cfd3a1809f99c
perryshe <perryshe.s21@gmail.com>
2026-06-05 13:45:42 +0300

    Revert "dnd_develop_fix_prisma_schema_path_in_entrypoint"
    This reverts commit 883471492f85539d32705f25c727423bb23c65a9.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 set -e
-SCHEMA="./prisma/schema.prisma"
 
 echo "Running Prisma generate..."
-npx prisma generate --schema="$SCHEMA"
+npx prisma generate
 
 echo "Running Prisma db push..."
-npx prisma db push --schema="$SCHEMA" --accept-data-loss
+npx prisma db push --accept-data-loss
 
 echo "Running seed..."
 npx tsx prisma/seed.ts
