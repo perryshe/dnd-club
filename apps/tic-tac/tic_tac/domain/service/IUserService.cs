7f175ad704b8b50ee857b55f53f83601a1a240a0
perryshe <perryshe.s21@gmail.com>
2026-06-09 21:48:27 +0300

    fix: duplicate using/namespace in IUserService.cs

--- apps/tic-tac/tic_tac/domain/service/IUserService.cs
+++ apps/tic-tac/tic_tac/domain/service/IUserService.cs
@@ -2,10 +2,6 @@ using tic_tac.domain.model;
 
 namespace tic_tac.domain.service;
 
-using tic_tac.domain.model;
-
-namespace tic_tac.domain.service;
-
 public interface IUserService
 {
     bool Register(string login, string password);
